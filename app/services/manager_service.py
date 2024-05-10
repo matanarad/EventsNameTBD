f08fc9ea68e0621c3c4c51869e3c538da80c6ea8
royhendel <46100559+royhendel@users.noreply.github.com>
2024-05-10 18:10:16 +0300

    boilerplate, waiting on template

--- app/services/manager_service.py
+++ app/services/manager_service.py
@@ -14,17 +14,20 @@ class ManagerService(TemplateService):
         self.table = Manager
 
     def get_events_by_manager(self):
+        #likely a super func
         pass
 
     def get_managers_by_event(self):
+        # likely a super func
         pass
 
     def add_manager(self, manager):
-        # slika
         manager_row = Manager(user_id=manager.user_id,
                               event_id=manager.event_id,)
         super().add_row(manager_row)
         return manager_row
 
-    def get_purchase_list(self, transaction_id):
-        pass
+    def remove_manager(self, manager):
+        manager_row = Manager(user_id=manager.user_id,
+                              event_id=manager.event_id, )
+        super().remove_row(manager_row)
