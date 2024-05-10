71a1706865b5725c93e6dcb47d691bc4d113a550
shachardavid <shachadavid2013@gmail.com>
2024-05-10 16:14:13 +0300

    added state to transaction

--- app/repositories/transaction_repository.py
+++ app/repositories/transaction_repository.py
@@ -2,6 +2,15 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship
 
 from .database import Base
+from enum import Enum
+
+
+class TransactionState(Enum):
+    APPROVED = 1
+    WAITING = 2
+    CANCELLED = 3
+    DECLINED = 4
+    ERROR = 5
 
 
 class Transaction(Base):
@@ -10,6 +19,7 @@ class Transaction(Base):
     transaction_id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'), index=True)
     event_id = Column(Integer, ForeignKey('events.id'), index=True)
+    state = Column(Integer, index=True)
     amount = Column(Integer, nullable=False)
     method = Column(String, nullable=False)
     date = Column(Date, nullable=False)
--- app/schemes/transaction_scheme.py
+++ app/schemes/transaction_scheme.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 class TransactionBase(BaseModel):
     user_id: int
     event_id: int
+    state: int
     ticket_type: str
     amount: int
     method: str
