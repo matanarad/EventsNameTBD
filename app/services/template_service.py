f31873c34b4f8f8ef0362f337ba6777c2e4e877c
shachardavid <shachadavid2013@gmail.com>
2024-05-10 18:32:17 +0300

    added recordby and multiple records by

--- app/services/template_service.py
+++ app/services/template_service.py
@@ -1,4 +1,6 @@
 from typing import Type, List, Generic, TypeVar
+
+from sqlalchemy import ColumnElement
 from sqlalchemy.orm import Session, Query
 
 from app.repositories.transaction_repository import Transaction
@@ -14,14 +16,20 @@ class TemplateService(Generic[T]):
     def get_all_records(self) -> List[Type[T]]:
         return self.db.query(T).all()
 
+    def get_record_by(self, field: ColumnElement, value) -> T:
+        return self.db.query(T).filter(field == value).first()
+
+    def get_multiple_records_by(self, field: ColumnElement, value) -> List[T]:
+        return self.db.query(T).filter(field == value).all()
+
     def add_row(self, row: T):
-        self.db.add(T)
+        self.db.add(row)
         self.db.commit()
-        self.db.refresh(T)
+        self.db.refresh(row)
         return T
 
     def remove_row(self, row):
         self.db.delete(row)
         self.db.commit()
-        self.db.refresh(T)
+        self.db.refresh(row)
         return T
