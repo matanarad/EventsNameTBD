12961dc8ff4b24220b58d3f0ab408606ee0d18fb
shachardavid <shachadavid2013@gmail.com>
2024-05-10 14:15:01 +0300

    template for user ready

---
+++ app/repositories/user.repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    phone_number = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_manager = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner")
