6f7ccab3410b49c1fed8b1f9c29c6d61377a2497
royhendel <46100559+royhendel@users.noreply.github.com>
2024-05-10 16:06:07 +0300

    added event and ticket repo

---
+++ app/repositories/event_repository.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DATETIME
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Event(Base):
+    __tablename__ = "event"
+    event_id = Column(Integer, primary_key=True, index=True)
+    creator_id = Column(Integer, nullable=False, index=True)
+    creation_date = Column(DATETIME, nullable=False)
+    event_date = Column(DATETIME, nullable=False, index=True)
+    Title = Column(DATETIME, nullable=False, index=True)
+    description = Column(String, nullable=False)
+    eventphoto_id = Column(Integer, nullable=False, default="/photopath/default.png")
