174d85ad67e2f23e2ac33a2c458a36ad58d2daeb
shachardavid <shachadavid2013@gmail.com>
2024-05-10 18:43:42 +0300

    added get records to transaction service

--- app/services/transaction_service.py
+++ app/services/transaction_service.py
@@ -11,17 +11,14 @@ class TransactionsService(TemplateService[Transaction]):
     def __init__(self):
         super().__init__()
 
-    def get_transaction_by_id(self, transaction_id: int) -> Type[Transaction]:
-        query: Query[Type[Transaction]] = self.db.query(Transaction).filter(Transaction.id == transaction_id)
-        return query.first()
+    def get_transaction_by_id(self, transaction_id: int) -> Transaction:
+        return self.get_record_by(Transaction.transaction_id, transaction_id)
 
-    def get_transactions_by_event_id(self, event_id: str) -> List[Type[Transaction]]:
-        query: Query[Type[Transaction]] = self.db.query(Transaction).filter(Transaction.event_id == event_id)
-        return query.all()
+    def get_transactions_by_event_id(self, event_id: str) -> List[Transaction]:
+        return self.get_multiple_records_by(Transaction.event_id, event_id)
 
-    def get_transactions_by_user_id(self, user_id: str) -> List[Type[Transaction]]:
-        query: Query[Type[Transaction]] = self.db.query(Transaction).filter(Transaction.user_id == user_id)
-        return query.all()
+    def get_transactions_by_user_id(self, user_id: str) -> List[Transaction]:
+        return self.get_multiple_records_by(Transaction.user_id, user_id)
 
     def create_transaction(self, transaction: TransactionCreate):
         # slika
