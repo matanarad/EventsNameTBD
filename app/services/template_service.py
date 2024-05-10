9c1060181b77831fde9881b7096f7cff561f3305
shachardavid <shachadavid2013@gmail.com>
2024-05-10 18:20:52 +0300

    finished basic service template

--- app/services/template_service.py
+++ app/services/template_service.py
@@ -5,20 +5,23 @@ from app.repositories.transaction_repository import Transaction
 from app.schemes.transaction_scheme import TransactionCreate
 
 T = TypeVar('T')
-TCS = TypeVar('TCS')
-TSS = TypeVar('TSS')
 
 
-class TemplateService(Generic[T, TCS, TSS]):
+class TemplateService(Generic[T]):
     def __init__(self):
         self.db = Session()
 
     def get_all_records(self) -> List[Type[T]]:
         return self.db.query(T).all()
 
-
-    def add_row(self, row):
-        pass
+    def add_row(self, row: T):
+        self.db.add(T)
+        self.db.commit()
+        self.db.refresh(T)
+        return T
 
     def remove_row(self, row):
-        pass
+        self.db.delete(row)
+        self.db.commit()
+        self.db.refresh(T)
+        return T
