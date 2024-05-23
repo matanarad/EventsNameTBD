82900d0e7ea335dd13fa654b68457da591345b26
Brosh82-B <brosh82.ploga.b@gmail.com>
2024-05-23 23:05:45 +0300

    new CrateEventPage

--- src/Pages/CreateEvent/CreateEvent.jsx
+++ src/Pages/CreateEvent/CreateEvent.jsx
@@ -1,21 +1,54 @@
 import { useState } from "react";
 import "./CreateEvent.css";
-import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import backIcon from "../../img/backIcon.svg";
 
+import React from "react";
+import { useForm, useFieldArray } from "react-hook-form";
+import config from "../../config";
+const questionsList = [
+  {
+    name: "Event Title",
+    type: "text",
+  },
+  {
+    name: "Event Date",
+    type: "date",
+  },
+  {
+    name: "Event Start Time",
+    type: "time",
+  },
+  {
+    name: "Event Description",
+    type: "text",
+  },
+  {
+    name: "Event Address",
+    type: "text",
+  },
+];
+
 function CreateEvent() {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [preview, setPreview] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
   const { userID } = useParams();
 
-  const { register, handleSubmit } = useForm();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "phoneNumbers",
+  });
+
   const onSubmit = (data) => {
     console.log(data);
   };
-
-  const [selectedFile, setSelectedFile] = useState(null);
-
-  const [preview, setPreview] = useState("");
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -27,59 +60,71 @@ function CreateEvent() {
       setPreview("");
     }
   };
-
-  const questionsList = [
-    {
-      name: "Event Title",
-      type: "text",
-    },
-    {
-      name: "Event Date",
-      type: "date",
-    },
-    {
-      name: "Event Time",
-      type: "time",
-    },
-    {
-      name: "Event Description",
-      type: "text",
-    },
-    {
-      name: "Event Address",
-      type: "text",
-    },
-    {
-      name: "Event minimum age",
-      type: "number",
-    },
-  ];
   return (
     <div className="CreateEvent">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Link to={`/MyEventsPage/${userID}`}>
-          <button className="return-bar">
-            <img className="icon" src={backIcon} alt="None" />
-            {/* <div>Return to event page</div> */}
-          </button>
-        </Link>
+      <Link to={`/MyEventsPage/${userID}`}>
+        <button className="return-bar">
+          <img className="icon" src={backIcon} alt="None" />
+          {/* <div>Return to event page</div> */}
+        </button>
+      </Link>
+      <form onSubmit={handleSubmit(onSubmit)} className="CreateEvent">
+        {/* Render questions from the questions list */}
         <div className="title">Create New Event</div>
-        {questionsList.map((question) => {
-          return (
-            <div className="input-container ic2" key={question.name}>
+        {questionsList.map((question, index) => (
+          <div className="input-container" key={question.name}>
+            <input
+              type={question.type}
+              {...register(question.name, { required: true })}
+              className={`input ${errors[question.name] ? "error" : ""}`}
+            />
+            <div className="cut">{question.name}</div>
+          </div>
+        ))}
+
+        {/* Input for list of phone numbers */}
+        <div style={{ marginTop: "2.5vh" }}>
+          <label>Add Managers By Phone Numbers</label>
+          {fields.map((field, index) => (
+            <div
+              key={index}
+              style={{
+                display: "grid",
+                gridTemplateColumns: "1fr max-content",
+                gap: "2vw",
+                marginTop: "2.5vh",
+              }}
+            >
               <input
-                {...register(question.name, { required: true })}
-                id={question.name}
-                className="input"
-                type={question.type}
-                placeholder=" "
+                type="tel"
+                className={`input ${
+                  errors.phoneNumbers?.[index] ? "error" : ""
+                }`}
+                {...register(`phoneNumbers.${index}`, { required: true })}
+                defaultValue={field.value} // Populate existing values
               />
-              <div className="cut">{question.name}</div>
-              <label className="placeholder">{question.name}</label>
+              <button
+                type="button"
+                onClick={() => remove(index)}
+                className="remove-button"
+              >
+                Remove
+              </button>
             </div>
-          );
-        })}
-        <div className="input-container ic2">
+          ))}
+          {fields.length < config.maxNumberOfMangers && (
+            <button
+              type="button"
+              onClick={() => append("")}
+              className="add-phone-number-button"
+            >
+              Add Phone Number
+            </button>
+          )}
+        </div>
+        {/* Input for image */}
+
+        <div className="input-container">
           <input
             {...register("image_file", { required: true })}
             id="file"
@@ -87,18 +132,19 @@ function CreateEvent() {
             type="file"
             onChange={handleFileChange}
           />
-          <div className="cut">{"Choose Event Image"}</div>
-          <label className="placeholder">{"Choose Event Image"}</label>
-
           <label htmlFor="file" className="custom-file-label">
             {selectedFile ? selectedFile.name : "Choose Event Image"}
           </label>
         </div>
+        {preview && (
+          <img src={preview} alt="Preview" className="preview-image" />
+        )}
+
+        {/* Submit button */}
         <button type="submit" className="submit">
           Submit
         </button>
       </form>
-      {preview && <img src={preview} alt="Preview" className="preview-image" />}
     </div>
   );
 }
--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const config = {
   url: "https://192.168.1.205:3000/",
   shareDataTitle: "ToDo Add massage Title",
   shareDataText: "ToDo Add massage",
+  maxNumberOfMangers: 5,
 };
 
 export default config;
